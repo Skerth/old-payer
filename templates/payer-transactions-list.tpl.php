7c98d86ceb171770f9a28df0cd60abb95cac8dfa
sklyatov <sklyatov@gmail.com>
2018-06-21 15:00:10 +0300

    #ffix payer-transactions-list.tpl add new template file for view transactions

---
+++ templates/payer-transactions-list.tpl.php
@@ -0,0 +1,21 @@
+<div id="content" class="clearfix">
+  <style>
+    table tr:hover td {
+      background: #e0e1db !important;
+    }
+    .transaction-amount:before {
+      font-size: 20px;
+      vertical-align: top;
+    }
+    .transaction-amount-0:before {
+      content: '⬆ ';
+      color: #ed541d;
+    }
+    .transaction-amount-1:before {
+      content: '⬇ ';
+      color: #008000;
+    }
+  </style>
+  <h2><?php print t('List of transactions'); ?></h2>
+  <?php print $page_data; ?>
+</div>
